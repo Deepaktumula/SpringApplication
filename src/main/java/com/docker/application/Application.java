d60f3ba2cb938adfcc651115e826151f5fa93be6
Deepaktumula <tumuladeepak.cse.2019@nist.edu>
2026-05-02 15:18:49 +0530

    Made changes

--- src/main/java/com/docker/application/Application.java
+++ src/main/java/com/docker/application/Application.java
@@ -9,7 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("----------------------------------");
-		System.out.println("Spring Application Started...");
+		System.out.println("Spring Application Started....");
 		System.out.println("----------------------------------");
 	}
 
